095c7e0ec76b5aab1ce8696cece71fc0045bc3ec
Tom <tom_lord@live.com>
2021-05-20 18:25:31 +1000

    some CSS and FUNCTIONING GAME!

--- game.js
+++ game.js
@@ -1,17 +1,27 @@
 //GLOBAL VARIABLES
 var startVsPlayer = document.querySelector('.vs-player');
-var startVsPc = document.querySelector('.vs-pc');
+var grid1 = document.querySelector('.grid1');
+var btnContainer = document.querySelector('.btn-container');
+var showResults = document.querySelector('.messages-div');
+var quitBtn = document.createElement('button');
+var playerOneIsX = document.querySelector('.player-is-x');
+var playerOneIsO = document.querySelector('.player-is-o');
+var mainMenu = document.querySelector('.main-menu-wrapper');
+var playAgainBtn = document.querySelector('.play-again-btn');
+var mainMenuBtn = document.querySelector('.main-menu-btn');
+var whosTurn = document.querySelector('.your-turn');
+var player1 = '';
+var player2 = '';
 var gameSquares = null;
-var showResults = document.querySelector('.results-div');
 var turnCounter = 0;
+var drawCounter = 9;
 var resultsArray = [];
-var playAgainBtn = document.createElement('button');
+
 
 
 
 
 // !GAME FUNCTIONALITY!
-
 // // FUNCTION TO ADD ONE TO TURNCOUNTER() {
 function addTurnCounter(){
     turnCounter = turnCounter + 1;
@@ -20,13 +30,13 @@ function addTurnCounter(){
 //determine if a clicked square should have an x or o and adds corresponding class to that square
 function determineXO() {
     if (turnCounter % 2 === 0) {
-        event.target.textContent = 'X';
+        event.target.innerHTML = '<img class="token" src="x-token-small.jpg" alt="X"></img>';
         event.target.classList.add('clicked-x');
-        showResults.textContent = "It's player ones turn";
+        whosTurn.textContent = `It's ${player2}s turn`;
         } else {
-            event.target.textContent = 'O';
+            event.target.innerHTML = '<img class="token" src="o-token-small.jpg" alt="X"></img>';
             event.target.classList.add('clicked-o');
-            showResults.textContent = "It's player twos turn";
+            whosTurn.textContent = `It's ${player1}s turn`;
         }
 }
 
@@ -34,47 +44,53 @@ function determineXO() {
 function determineWinner(){
     for (var i = 0; i < resultsArray.length; i++) {
         if (resultsArray[i][0].classList.contains('clicked-o') && resultsArray[i][1].classList.contains('clicked-o') && resultsArray[i][2].classList.contains('clicked-o') ){
-            turnCounter = 10;
-            showResults.textContent = 'Player one wins!';
-            playAgain();
+            turnCounter = 11;
+            whosTurn.textContent = `${player2} wins!`;
         } else if (resultsArray[i][0].classList.contains('clicked-x') && resultsArray[i][1].classList.contains('clicked-x') && resultsArray[i][2].classList.contains('clicked-x')){
-            turnCounter = 10;
-            showResults.textContent = ('Player two wins!')
-            playAgain();
+            turnCounter = 11;
+            whosTurn.textContent = (`${player1} wins!`)
         } 
     }
 }
 
 // HANDLES DRAW SCENARIO
-function itsADraw(){
-    if (turnCounter === 9){
-        showResults.textContent = "It's a draw";
-        playAgain();
+function determineDraw(){
+    if (turnCounter === drawCounter){
+        whosTurn.textContent = "It's a draw";
     }
 }
 
-//creates a button which resets the game on click
-function playAgain(){
-    if (turnCounter === 9 || turnCounter === 10){
-        document.body.appendChild(playAgainBtn);
-        playAgainBtn.innerText = 'Play Again?';
-        playAgainBtn.classList.add('play-again-btn');
-        playAgainBtn.addEventListener('click', resetGame);
-    }
+//creates a play again button which resets the game on click and a return to menu button which quits the game
+function createPlayAgainMainMenuBtns(){
+    btnContainer.classList.toggle('display-none');
+    playAgainBtn.addEventListener('click', resetGame);
+    mainMenuBtn.addEventListener('click', quitGame);
 }
 
 // resets game elements to default for a new game
 function resetGame() {
-    document.body.removeChild(playAgainBtn);
     for (var i = 0; i < gameSquares.length; i++){
         gameSquares[i].classList.remove('clicked-o', 'clicked-x');
         gameSquares[i].textContent = '';
-        turnCounter = 0;
-        showResults.textContent = "It's player ones turn";
-        
+        if (drawCounter === 9 ) {
+            turnCounter = 0;
+        } else if (drawCounter === 10 ) {
+            turnCounter = 1;
+        }
+        whosTurn.textContent = "It's Player 1s turn";        
     }
 }
 
+function quitGame() {
+    destroyGrid();
+    grid1.classList.toggle('div-wrapper');
+    showResults.classList.toggle('display-none');
+    whosTurn.classList.toggle('display-none');
+    mainMenu.classList.toggle('display-none');
+    btnContainer.classList.toggle('display-none');
+    whosTurn.textContent = "It's Player 1s turn";
+    turnCounter = 0;
+}
 
 // EVALUATES IF THE TURN IS VALID, AND IF SQUARE SHOULD HAVE AN X OR O.
 function turnTaken() {
@@ -83,21 +99,33 @@ function turnTaken() {
             addTurnCounter();
             determineXO();
             determineWinner();
-            itsADraw();
+            determineDraw();
         }
     }
 }
 
 
 // creates the 3x3 grid after an option is selected on start menu. adding classes and event listeners to each box
-function createGrid() {
+function createGameBoard() {
+    createPlayAgainMainMenuBtns();
+    showResults.classList.toggle('display-none');
+    whosTurn.classList.toggle('display-none');
+    mainMenu.classList.toggle('display-none'); 
+    grid1.classList.toggle('div-wrapper');
     for (var i = 1; i < 10; i++) {
         var createGrid = document.createElement('div');
-        document.querySelector('.div-wrapper').appendChild(createGrid);
+        grid1.appendChild(createGrid);
         createGrid.setAttribute('class', 'game-squares box' + i);
         var createdSquare = document.querySelector('.box' + i)
         createdSquare.addEventListener('click', handleClick);
         gameSquares = document.querySelectorAll('.game-squares');
+    }
+}
+
+// removes the grid
+function destroyGrid() {
+    for (var i = 0; i < 9; i++){
+    gameSquares[i].remove();
     }
 }
 
@@ -149,12 +177,20 @@ function generateWinScenarios(){
 }
 
 // hides start menu buttons and initiates creation of grid and win scenarios.
-function startPlayerGame(){
-    document.querySelector('.vs-player').style.display = 'none';
-    document.querySelector('.vs-pc').style.display = 'none';
-    showResults.textContent = "It's player ones turn";
-    createGrid();
-    generateWinScenarios();    
+function startPlayerX(){
+    createGameBoard();
+    generateWinScenarios();
+    turnCounter = 1;
+    drawCounter= 10;
+    player1 = 'Player 1';
+    player2 = 'Player 2';  
+}
+
+function startPlayerO(){
+    createGameBoard();
+    generateWinScenarios();
+    player1 = 'Player 2';
+    player2 = 'Player 1';
 }
 
 // HANDLE SQUARE CLICKED EVENT
@@ -162,5 +198,5 @@ function handleClick() {
     turnTaken();  
 }
 
-startVsPlayer.addEventListener('click', startPlayerGame);
-
+playerOneIsX.addEventListener('click', startPlayerX);
+playerOneIsO.addEventListener('click', startPlayerO);
